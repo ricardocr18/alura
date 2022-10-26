bc202bba41fd944b1964240f1fe65409d33235f0
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-10-26 00:55:50 -0300

    atualização da trilha JavaScript BackEnd

---
+++ javaScriptParaBackEnd/funcao/arrowFunction.js
@@ -0,0 +1,16 @@
+//inscrita normal da função
+function apreentar(nome) {
+    return `meu nome é ${nome}`
+}
+
+//Agora com arrowFunction
+const apresentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 +num2;
+
+const somaNumeroPequeno = (num1, num2) => {
+    if (num1 > 10 || num2 > 10){
+        return "somente nmeros de 1 a 9"
+    }else {
+        return num1 + num2
+    }
+}
---
+++ javaScriptParaBackEnd/funcao/expressoesFuncao.js
@@ -0,0 +1,6 @@
+//expressão de função (não tem nome geralmente)
+
+const soma = function (num1, num2){
+    return num1 + num2
+}
+console.log(soma(1,2))
