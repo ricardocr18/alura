1229ce6d1228ba36175059688a7b89ea814671fb
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-10-27 23:34:17 -0300

    novas atividades criadas de array

---
+++ javaScriptParaBackEnd/array/for.js
@@ -0,0 +1,6 @@
+//usando o FOR um laço de repetição
+const numeros = [100, 200, 300,400, 500,600]
+
+for (let i = 0; i < numeros.length; i++){
+    console.log(i, numeros[i])
+}
--- javaScriptParaBackEnd/array/localizarAlunos.js
+++ javaScriptParaBackEnd/array/localizarAlunos.js
@@ -1,9 +1,19 @@
-//Aqui nesse exemplo junto dois arrays em um só e depois faço uma pesquisa
-const alunos = [ 'Ricardo', 'Raquel', 'Bruno', 'Fernanda']
+//Aqui nesse exemplo junto dois arrays em um só.
+//Depois usa o INCLUIDES (retrona um booleano True ou False) para fazer uma busca pelo tipo de dados que no caso é o nome, 
+//já o uso do INDEXOF, traz o indice onde o nome está no array
+const alunos = ['Ricardo', 'Raquel', 'Bruno', 'Fernanda']
 const mediaDosAlunos = [10,7,9,6]
 
 let listaDeNotasEAlunos = [alunos, mediaDosAlunos]
 
 const exibeNomeNota = (nomeDoAluno) => {
-    if (listaDeNotasEAlunos[0])
+    if (listaDeNotasEAlunos[0].includes(nomeDoAluno)){
+        let indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno)
+        return listaDeNotasEAlunos[0][indice] + ', sua media é ' +
+        listaDeNotasEAlunos[1][indice]
+    } else {
+        return "Aluno não esta cadastrado"
+    }
 }
+
+console.log(exibeNomeNota("Bruno"))
