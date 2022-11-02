aba5aba154857c0b875edd41095609648765c5a6
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-02 19:41:27 -0300

    atividades com algoritmoDeOrdenação PArteII

--- javaScriptParaBackEnd/algoritmosOrdenacaoParteII/index.js
+++ javaScriptParaBackEnd/algoritmosOrdenacaoParteII/index.js
@@ -1,20 +1,28 @@
-const {edGalho, edFolla} = require("./array");
+const {edGalho, edFolha} = require("./array");
 
-function juntaListas(lista1, listas2){
+function juntaListas(lista1, lista2){
     let listaFinal = [];
     let posicaoAtualLista1 = 0;
     let posicaoAtualLista2 = 0;
     let atual = 0;
 
-    while (posicaoAtualLista1 < lista1.length &&posicaoAtualLista2 < listas2.length){
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length){
         let produtoAtualLista1 = lista1[posicaoAtualLista1];
         let produtoAtualLista2 = lista2[posicaoAtualLista2];
 
-        if (produtoAtualLista1.preco < produtoAtualLista2){
-            listaFinal[atual] = produtoAtualLista1
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco){
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++
         }
+
+        atual++
 
     }
 
     return listaFinal
 }
+
+console.log(juntaListas(edGalho, edFolha))
