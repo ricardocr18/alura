49f335797a68c11355921ac99fa6014202150571
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-02 22:27:15 -0300

    criando biblioteca com NodeJS

---
+++ javaScriptParaBackEnd/nodeJSCriandoBiblioteca/index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+//const chalk = require ('chalk')
+
+console.log(chalk.blue("Olá mudo"))
+console.log(`São geralmente recuperados a partir de um objeto
+ [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList)
+  que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento,
+   a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer)
+    utilizado em operações de arrastar e soltar, ou a partir da API mozGetAsFile() 
+    em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement).`)
