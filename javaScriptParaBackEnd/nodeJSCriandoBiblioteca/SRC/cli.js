12fd63ca47cbe88e6f545d2e25dccf0e62d11c5f
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-05 07:31:41 -0300

    criando biblioteca com NodeJS

--- javaScriptParaBackEnd/nodeJSCriandoBiblioteca/SRC/cli.js
+++ javaScriptParaBackEnd/nodeJSCriandoBiblioteca/SRC/cli.js
@@ -6,9 +6,22 @@ import pegarArquivo from "./index.js"
 const caminho = process.argv
 // console.log(caminho)Aqui vejo tudo que o process.argv me retorna
 
-async function processaTexto(caminho) {
-    const resultado = await pegarArquivo(caminho[2])
-    console.log(chalk.yellow('lista de links'), resultado)
+function imprimeLista(resultado){
+    console.log(chalk.yellow('lista de inks'), resultado)
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2]
+    if (fs.lstatSync(caminho).isFile()){
+        const resultado = await pegarArquivo(argumentos[2])
+        imprimeLista(resultado)
+    } else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) =>{
+            const lista = await pegarArquivo(`${caminho}/${nomeDeArquivo}`)
+            imprimeLista(lista)
+        })        
+    }    
 }
 
 processaTexto(caminho)
