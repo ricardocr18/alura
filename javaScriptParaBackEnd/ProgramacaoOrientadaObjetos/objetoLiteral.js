7251528c3389961f79615fc94f27c8b1e603399f
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-08 23:08:30 -0300

    criando OO com javascript

---
+++ javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/objetoLiteral.js
@@ -0,0 +1,10 @@
+const user = {
+    nome: "Dede",
+    email: "dede@email.com",
+    nascimento: "2001/01/01",
+    role: "admin",
+    ativo: true,
+    exibirInfos: function (){
+        console.log
+    }
+}
