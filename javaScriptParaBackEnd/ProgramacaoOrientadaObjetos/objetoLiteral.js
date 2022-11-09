f88bce5555e3cfa5ce64781716344535f52e096c
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-08 23:39:51 -0300

    criando OO com javascript

--- javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/objetoLiteral.js
+++ javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/objetoLiteral.js
@@ -2,11 +2,26 @@ const user = {
     nome: "Dede",
     email: "dede@email.com",
     nascimento: "2001/01/01",
-    role: "admin",
+    role: "estudante",
     ativo: true,
     exibirInfos: function (){
         console.log(this.nome, this.email)
     }
 }
 
-user.exibirInfos()
+const admin = {
+    nome: "Didi",
+    email: "didi@email.com",
+    nascimento: "2002/02/02",
+    role: "admin",
+    criarCurso(){
+        console.log('Curso criado')
+    }
+}
+
+//nessa linha abaixo, vinculamos o exibirInfos de const user, com o const admin
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
+
+//user.exibirInfos()
