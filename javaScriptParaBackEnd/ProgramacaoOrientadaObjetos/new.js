de92bf8dff592f5bdc31005316d05ff577555225
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-09 00:24:39 -0300

    criando function construtora

--- javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/new.js
+++ javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/new.js
@@ -9,5 +9,10 @@ function User(nome, email){
 
 //aqui na linha de baixo estamos usando uma function construtor com o NEW
 //User será o prototipo da nova const que foi criada novoUser
-const novoUser = new User("Mussum", "mussum@email.com")
-console.log(novoUser.exibirInfos())
+// const novoUser = new User("Mussum", "mussum@email.com")
+// console.log(novoUser.exibirInfos())
+
+function Admin(role){
+User.call(this, 'Zacarias', 'zacarias@email.com')
+this.role = role || 'estudante'
+}
