7a46c1fa4c7f69137fc0cfc609609329906e190f
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-09 17:36:00 -0300

    criando function construtora

--- javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/new.js
+++ javaScriptParaBackEnd/ProgramacaoOrientadaObjetos/new.js
@@ -1,18 +1,55 @@
-function User(nome, email){
-    this.nome = nome,
-    this.email = email
+//TRÊS FORMAS DE FAZER O PROTOTIPO
 
-    this.exibirInfos = function(){
-        return `${this.nome}, ${this.email}`
-    }
-}
+
+// function User(nome, email){
+//     this.nome = nome,
+//     this.email = email
+
+//     this.exibirInfos = function(){
+//         return `${this.nome}, ${this.email}`
+//     }
+// }
 
 //aqui na linha de baixo estamos usando uma function construtor com o NEW
 //User será o prototipo da nova const que foi criada novoUser
 // const novoUser = new User("Mussum", "mussum@email.com")
 // console.log(novoUser.exibirInfos())
 
-function Admin(role){
-User.call(this, 'Zacarias', 'zacarias@email.com')
-this.role = role || 'estudante'
+// function Admin(role){
+// User.call(this, 'Zacarias', 'zacarias@email.com')
+// this.role = role || 'estudante'
+// }
+
+// Admin.prototype = Object.create(User.prototype)
+// const novoUser = new Admin('admin')
+// console.log(novoUser.exibirInfos())
+// console.log(novoUser.role)
+
+//===========================================================
+
+// const user = { 
+//     exibirInfos: function(nome){
+//         return nome
+//     }
+// }
+
+// const novoUser = Object.create(user)
+// console.log(novoUser.exibirInfos("Mussum"))
+// console.log(user.isPrototypeOf(novoUser)) //aqui eu apeas estou confirmando que novoUser usa o User e retorna TRUE
+
+//====================================================
+
+const user = { 
+    init: function(nome, email){
+        this.nome = nome,
+        this.email = email
+    },
+
+    exibirInfos: function(){
+        return this.nome
+    }
 }
+
+const novoUser = Object.create(user)
+novoUser.init('Mussum ', "mussum@email.com")
+console.log(novoUser.exibirInfos())
