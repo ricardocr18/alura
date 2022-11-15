ef9f957b896831d4f5f56dfe406768071dc6b7a1
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-11-15 20:58:05 -0300

    nodejs com API rest com express e mongoDB

--- trilhaNodejsComExpress/nodejsExpressMongoDB/server.js
+++ trilhaNodejsComExpress/nodejsExpressMongoDB/server.js
@@ -1,2 +1,18 @@
 const http = require ('http')
 const port = 3000; 
+
+const rotas = {
+    '/': 'Cursos de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listem de autores',
+    '/sobre': 'Informações sobre a livraria'
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(rotas[req.url])
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
